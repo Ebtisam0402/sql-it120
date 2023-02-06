f6a724d38821a4cc71aae78fd8175021612cf4c3
Ebtisam <ebtisam21@gmail.com>
2023-02-05 20:09:41 -0800

    add files

---
+++ create-tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE contact (
+    contactID INTEGER,
+    name VARCHAR
+);
+
+CREATE TABLE phone (
+    phonenumber VARCHAR,
+    contactID INTEGER,
+    type VARCHAR
+);
---
+++ populate-tables.sql
@@ -0,0 +1,10 @@
+INSERT INTO contact(contactID, name)
+VALUES
+(666888, 'Azuz'),
+(999123, 'Manar');
+
+INSERT INTO phone(phonenumber, contactID, type)
+VALUES
+(2069991011, 666888, 'IT'),
+(2061233211, 999123, 'Nurse'),
+(2061111234, 999123, 'Nurse');
